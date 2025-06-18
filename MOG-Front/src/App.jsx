f94116d5273d26536e645ce25750d1aeba0894ca
김원진 <76833065+oen0thera@users.noreply.github.com>
2025-06-18 20:45:56 +0900

    feat : 초기 프로토타입 웹페이지 구성 및 컴포넌트 추가
    1. 초기 프로토타입 웹페이지 구성
    - vite + react 로 프레임워크 구성
    - javascript 언어 사용
    - .gitignore 추가
    - .prettierrc 추가
    - alias 추가 : import '@/...' -> import 'src/...'
    - 의존성 패키지 설치 : react-router-dom, eslint, prettier, react-bootstrap
    - 화면 예시 내 화면 구성 및 파일 추가
    - bootstrap 연동
    - bootstrap.min.css 전역 css 적용
    
    2. 컴포넌트 추가
    - 메인 페이지 내 요소들 분할하여 컴포넌트로 구성
      - GNB : 상단 nav 바 전역으로 설정되도록 App.jsx 내 최상위에 구성
      - Header : 배너 컴포넌트
      - Image : 이미지를 위해 사용되는 컴포넌트
        - className : bootstrap에 사용되는 class 이름
        - src : 이미지 경로 입력 기본적으로 assets/img/로 설정 되어있으므로 해당 경로 이후 디렉토리 경로 입력

---
+++ MOG-Front/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import '@/assets/bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home/Home';
+import GNB from './components/GNB/GNB';
+
+function App() {
+  return (
+    <div>
+      <GNB />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ MOG-Front/src/components/GNB/GNB.jsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+
+export default function GNB() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <span>제목예시(홈 버튼)</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i className="fas fa-bars ms-1"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="services">
+                로그인
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="portfolio">
+                기록
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="about">
+                통계
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="team">
+                사용자 화면
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
