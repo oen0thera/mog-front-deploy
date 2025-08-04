3147227d0b4138e05416387840d1d9b5b527970e
oen0thera <wjsj1015@naver.com>
2025-08-04 11:30:02 +0000

    deploy : 누락된 json 서버 api url 추가
    1. 누락된 json 서버 api url 추가

--- MOG-Front/src/config/constants.js
+++ MOG-Front/src/config/constants.js
@@ -1,6 +1,6 @@
 export const URL = {
-  ROUNTINE: 'http://localhost:3002/routine',
-  ROUTINEDETAIL: 'http://localhost:3002/routine_detail',
-  ROUTINERESULT: 'http://localhost:3002/routine_result',
+  ROUNTINE: 'https://mogapi.kro.kr/json/routine',
+  ROUTINEDETAIL: 'https://mogapi.kro.kr/json/routine_detail',
+  ROUTINERESULT: 'https://mogapi.kro.kr/json/routine_result',
   REALDATA: 'https://mogapi.kro.kr/api/v1/routine',
 };
